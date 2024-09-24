2da0796b6e2cb4f44affb8bb7f83c25c75f365e4
Julian K. Arni <jkarni@gmail.com>
2024-09-24 19:47:32 +0200

    Initial commit

---
+++ exe/Main.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Main where
+
+import GHC.Generics (Generic)
+import Network.Wai.Handler.Warp (run)
+import Servant
+import Servant.Server.Generic
+import System.Environment
+
+main :: IO ()
+main = do
+  [port] <- getArgs
+  run (read port) $ genericServe server
+
+data API route = API
+  {endpoint :: route :- Get '[JSON] Int}
+  deriving (Generic)
+
+server :: API AsServer
+server =
+  API
+    { endpoint = pure 1781
+    }
